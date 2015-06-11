00ef075007d64556d719c0f3763b535951ce904d
millanek <millanek@gmail.com>
2015-06-11 17:37:03 +0100

    debug

--- process_vcf_abba_baba.cpp
+++ process_vcf_abba_baba.cpp
@@ -101,6 +101,16 @@ inline void incrementDnumDdenomFrequency(const ThreeSetCounts& c, ABBA_BABA_Freq
         } else {
             thisF_d_denom = ((1-c.set1daAF)*c.set3daAF*c.set3daAF) - (c.set1daAF*(1-c.set3daAF)*c.set3daAF);
         }
+        
+        if (thisDnumerator/thisF_d_denom > 1 || thisDnumerator/thisF_d_denom < -1) {
+            std::cerr << "D num" << thisDnumerator << std::endl;
+            std::cerr << "f_d denom" << thisF_d_denom << std::endl;
+            std::cerr << "p1" << c.set1daAF << std::endl;
+            std::cerr << "p2" << c.set2daAF << std::endl;
+            std::cerr << "p3" << c.set3daAF << std::endl;
+            
+        }
+        
         res.f_d_denominator += thisF_d_denom; res.window_f_d_denominator += thisF_d_denom; res.lastVarsF_d_denom += thisF_d_denom;
         
         if (c.set3daAF == 1) {
