312aff7ea5dfcb95ad394b56ed7a83490f6b4d2d
millanek <millanek@gmail.com>
2017-01-14 02:39:08 +0100

    debug

--- process_vcf_coding_sequences.cpp
+++ process_vcf_coding_sequences.cpp
@@ -463,18 +463,18 @@ void getStatsBothPhasedHaps(const std::vector<std::string>& allSeqs, const std::
     }
     
     // std::cerr << "Collecting gene sequence statistics...." << std::endl;
-    std::vector<std::vector<double> > N_d_jk; initialize_matrix_double(N_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > N_jk; initialize_matrix_double(N_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > S_d_jk; initialize_matrix_double(S_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > S_jk; initialize_matrix_double(S_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H2N_d_jk; initialize_matrix_double(H2N_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H2N_jk; initialize_matrix_double(H2N_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H2S_d_jk; initialize_matrix_double(H2S_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H2S_jk; initialize_matrix_double(H2S_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H1H2N_d_jk; initialize_matrix_double(H1H2N_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H1H2N_jk; initialize_matrix_double(H1H2N_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H1H2S_d_jk; initialize_matrix_double(H1H2S_d_jk, (int)altCodons.size());
-    std::vector<std::vector<double> > H1H2S_jk; initialize_matrix_double(H1H2S_jk, (int)altCodons.size());
+    std::vector<std::vector<double> > N_d_jk; initialize_matrix_double(N_d_jk, numSamples);
+    std::vector<std::vector<double> > N_jk; initialize_matrix_double(N_jk, numSamples);
+    std::vector<std::vector<double> > S_d_jk; initialize_matrix_double(S_d_jk, numSamples);
+    std::vector<std::vector<double> > S_jk; initialize_matrix_double(S_jk, numSamples);
+    std::vector<std::vector<double> > H2N_d_jk; initialize_matrix_double(H2N_d_jk, numSamples);
+    std::vector<std::vector<double> > H2N_jk; initialize_matrix_double(H2N_jk, numSamples);
+    std::vector<std::vector<double> > H2S_d_jk; initialize_matrix_double(H2S_d_jk, numSamples);
+    std::vector<std::vector<double> > H2S_jk; initialize_matrix_double(H2S_jk, numSamples);
+    std::vector<std::vector<double> > H1H2N_d_jk; initialize_matrix_double(H1H2N_d_jk, numSamples);
+    std::vector<std::vector<double> > H1H2N_jk; initialize_matrix_double(H1H2N_jk, numSamples);
+    std::vector<std::vector<double> > H1H2S_d_jk; initialize_matrix_double(H1H2S_d_jk, numSamples);
+    std::vector<std::vector<double> > H1H2S_jk; initialize_matrix_double(H1H2S_jk, numSamples);
     for (string::size_type i = 0; i != allSeqs[0].length(); i++) {
         for (std::vector<std::string>::size_type j = 0; j != allSeqs.size(); j++) {
             altCodons[j] += allSeqs[j][i];
@@ -503,10 +503,15 @@ void getStatsBothPhasedHaps(const std::vector<std::string>& allSeqs, const std::
     double sumPn = 0;
     double sumPs = 0;
     // Add the within H1 and within H2 comparisons
-    for (std::vector<std::string>::size_type j = 0; j != altCodons.size() - 1; j++) {
+    for (std::vector<std::string>::size_type j = 0; j != numSamples - 1; j++) {
         std::cerr << "j = " << j << "; sumPn: " << sumPn << "; sumPs:" << sumPs << std::endl;
-        for (std::vector<std::string>::size_type k = j+1; k != altCodons.size(); k++) {
-            double pN_jk = N_d_jk[j][k]/N_jk[j][k]; sumPn = sumPn + pN_jk;
+        for (std::vector<std::string>::size_type k = j+1; k != numSamples; k++) {
+            double pN_jk = N_d_jk[j][k]/N_jk[j][k];
+            if (isnan(pN_jk)) {
+                std::cerr << "j = " << j << "k = " << k << std::endl;
+                std::cerr << "N_d_jk[j][k] = " << N_d_jk[j][k] << "N_jk[j][k] = " << N_jk[j][k] << std::endl;
+            }
+            sumPn = sumPn + pN_jk;
             double pS_jk = S_d_jk[j][k]/S_jk[j][k]; sumPs = sumPs + pS_jk;
             double H2pN_jk = H2N_d_jk[j][k]/H2N_jk[j][k]; sumPn = sumPn + H2pN_jk;
             double H2pS_jk = H2S_d_jk[j][k]/H2S_jk[j][k]; sumPs = sumPs + H2pS_jk;
