6fe0fb146eb0990c3250924c525514ab7b563355
millanek <millanek@gmail.com>
2018-06-29 12:39:26 +0200

    better nChooseK calculation

--- evo_Dmin.cpp
+++ evo_Dmin.cpp
@@ -126,6 +126,20 @@ void getSetVariantCounts(GeneralSetCounts* c, const std::vector<std::string>& ge
     }
 }
 
+inline unsigned nChoosek( unsigned n, unsigned k )
+{
+    if (k > n) return 0;
+    if (k * 2 > n) k = n-k;
+    if (k == 0) return 1;
+    
+    int result = n;
+    for( int i = 2; i <= k; ++i ) {
+        result *= (n-i+1);
+        result /= i;
+    }
+    return result;
+}
+
 int DminMain(int argc, char** argv) {
     parseDminOptions(argc, argv);
     string line; // for reading the input files
@@ -156,7 +170,7 @@ int DminMain(int argc, char** argv) {
             // std::cerr << it->first << std::endl;
         }
     } std::cerr << "There are " << species.size() << " sets (excluding the Outgroup)" << std::endl;
-    int nCombinations = choose((int)species.size(),3);
+    int nCombinations = nChoosek((int)species.size(),3);
     std::cerr << "Going to calculate " << nCombinations << " Dmin values" << std::endl;
     
     
