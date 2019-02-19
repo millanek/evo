675215a74d94f75f5c49ce3c44befea63c845fcc
millanek <millanek@gmail.com>
2019-02-19 14:43:33 +0100

    little bug fix

--- evo_FstAgainstAll.cpp
+++ evo_FstAgainstAll.cpp
@@ -264,11 +264,11 @@ void parseFstGlobaloptions(int argc, char** argv) {
         }
     }
     
-    if (argc - optind < 3) {
+    if (argc - optind < 2) {
         std::cerr << "missing arguments\n";
         die = true;
     }
-    else if (argc - optind > 3)
+    else if (argc - optind > 2)
     {
         std::cerr << "too many arguments\n";
         die = true;
@@ -282,5 +282,4 @@ void parseFstGlobaloptions(int argc, char** argv) {
     // Parse the input filenames
     opt::vcfFile = argv[optind++];
     opt::setsFile = argv[optind++];
-    opt::PBStriosFile = argv[optind++];
 }
