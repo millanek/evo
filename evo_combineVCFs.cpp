37fc9fcaf8b750a7ddb7c905c561f985a6df68fa
millanek <millanek@gmail.com>
2019-04-01 14:55:40 +0200

    output merged VCF tracks..

--- evo_combineVCFs.cpp
+++ evo_combineVCFs.cpp
@@ -7,13 +7,14 @@
 //
 
 #include "evo_combineVCFs.h"
+#include "process_vcf_annotation_tools.h"
 
 #define SUBPROGRAM "vcf-comb"
 
 #define DEBUG 1
 
 static const char *VCF_COMB_USAGE_MESSAGE =
-"Usage: " PROGRAM_BIN " " SUBPROGRAM " [OPTIONS] VCF_FILE1.vcf VCF_FILE2.vcf REF_1.fa REF2_IN_REF1COORDS.fa\n"
+"Usage: " PROGRAM_BIN " " SUBPROGRAM " [OPTIONS] VCF_FILE1.vcf VCF_FILE2.vcf REF_1.fa REF2_IN_REF1COORDS.fa MAPPABILITY_UNION.bed\n"
 "Generates a joint VCF file from two VCFs that have different samples and were originally called against two different references\n"
 "This is done per-chromosome\n"
 "\n"
@@ -37,17 +38,22 @@ namespace opt
     static string vcfFile2;
     static string refFile1;
     static string refFile2;
-    static string out = "";
+    static string mappabilityFile;
+    static string out = "Joined_VCF";
 }
 
 int VCFcombMain(int argc, char** argv) {
     parseVCFcombOptions(argc, argv);
+    const string refAllele = "0/0"; const string altAllele = "1/1";
     string line; // for reading the input files
     
     std::istream* vcfFile1 = createReader(opt::vcfFile1.c_str());
     std::istream* vcfFile2 = createReader(opt::vcfFile2.c_str());
     std::ifstream* refFile1 = new std::ifstream(opt::refFile1.c_str());
     std::ifstream* refFile2 = new std::ifstream(opt::refFile2.c_str());
+    
+    string extraMaskFileName = opt::out + "_extraMask.bed";
+    std::ofstream* extraMaskFile = new std::ofstream(extraMaskFileName.c_str());
     
     // Read in the whole reference sequences
     string refSeq1; string refSeq2; refSeq1.reserve(50000000); refSeq2.reserve(50000000);
@@ -66,18 +72,103 @@ int VCFcombMain(int argc, char** argv) {
     // Now go through the vcf and add the AA fields
     while (getline(*vcfFile1, line)) {
         if (line[0] == '#' && line[1] == '#') { header1.push_back(line); }
-        else if (line[0] == '#' && line[1] == 'C') { samples1 = split(line, '\t'); }
-        else {
+        else if (line[0] == '#' && line[1] == 'C') {
+            header1.push_back(line);
+            fields = split(line, '\t');
+            for (std::vector<std::string>::size_type i = NUM_NON_GENOTYPE_COLUMNS; i != fields.size(); i++) {
+                samples1.push_back(fields[i]);
+            }
+        } else {
             fields = split(line, '\t');
             VCF1[atoi(fields[1].c_str())] = line;
         }
     }
     while (getline(*vcfFile2, line)) {
         if (line[0] == '#' && line[1] == '#') { header2.push_back(line); }
-        else if (line[0] == '#' && line[1] == 'C') { samples2 = split(line, '\t'); }
-        else {
+        else if (line[0] == '#' && line[1] == 'C') {
+            header2.push_back(line); fields = split(line, '\t');
+            for (std::vector<std::string>::size_type i = NUM_NON_GENOTYPE_COLUMNS; i != fields.size(); i++) {
+                samples2.push_back(fields[i]);
+            }
+        } else {
             fields = split(line, '\t');
             VCF2[atoi(fields[1].c_str())] = line;
+        }
+    }
+    
+    std::ifstream* accessibleGenomeBed = new std::ifstream(opt::mappabilityFile.c_str());
+    std::cerr << "Loading the accessible genome annotation:" << std::endl;
+    AccessibleGenome* ag = new AccessibleGenome(accessibleGenomeBed);
+    std::cerr << "Done" << std::endl;
+    
+    
+    std::cout << header1[0] << std::endl;
+    std::cout << header1[1] << std::endl;
+    std::cout << header1.back(); print_vector_stream(samples2, std::cout);
+    
+    for (int i = 0; i < chrRef1.length(); i++) {
+        int pos = i+1;
+        if (ag->findIfBPaccessible(chrRef1, i+1) == false) continue;
+        if (refSeq1[i] == 'N' || refSeq2[i] == 'N') {
+            *extraMaskFile << chrRef1 << "\t" << i << "\t" << i+1 << std::endl;
+            continue;
+        }
+        if (refSeq1[i] == refSeq2[i]) {
+            if (VCF1.count(i) == 0 && VCF2.count(i) == 0) continue;
+            else if (VCF1.count(pos) == 1) {
+                if (VCF2.count(pos) == 0) {
+                    std::cout << VCF1[pos]; for (int j = 0; j < samples2.size(); j++) { std::cout << "\t" << refAllele; }
+                    std::cout << std::endl;
+                }
+                if (VCF2.count(pos) == 1) {
+                    fields = split(VCF1[pos], '\t'); string altAllele1 = fields[4];
+                    fields = split(VCF2[pos], '\t');
+                    if (altAllele1 == fields[4]) {
+                        std::vector<string> genotypes(fields.begin()+NUM_NON_GENOTYPE_COLUMNS,fields.end());
+                        std::cout << VCF1[pos]; print_vector_stream(genotypes, std::cout);
+                    } // else this would be multiallelic
+                }
+            }
+            else if (VCF2.count(pos) == 1) {
+                fields = split(VCF2[pos], '\t');
+                std::vector<string> genotypes(fields.begin()+NUM_NON_GENOTYPE_COLUMNS,fields.end());
+                for (int j = 0; j < NUM_NON_GENOTYPE_COLUMNS; j++) { std::cout << fields[j] << "\t"; }
+                for (int j = 0; j < samples1.size(); j++) { std::cout << refAllele << "\t"; }
+                print_vector_stream(genotypes,std::cout);
+            }
+        } else // refSeq1[i] != refSeq2[i]
+        {
+            if (VCF1.count(pos) == 0 && VCF2.count(pos) == 0) {
+                std::cout << chrRef1 << "\t" << pos << "\t.\t" << refSeq1[i] << "\t" << refSeq2[i] << "\t1000\tPASS\tAC=" << samples2.size()*2 << "\tGT";
+                for (int j = 0; j < samples1.size(); j++) { std::cout << "\t" << refAllele; }
+                for (int j = 0; j < samples2.size(); j++) { std::cout << "\t" << altAllele; }
+                std::cout << std::endl;
+            }
+            else if (VCF1.count(pos) == 1) {
+                fields = split(VCF1[pos], '\t'); string altAllele1 = fields[4];
+                if (altAllele1[0] != refSeq2[i]) continue; // This would be multiallelic
+                
+                if (VCF2.count(pos) == 0) {
+                    std::cout << VCF1[pos]; for (int j = 0; j < samples2.size(); j++) { std::cout << "\t" << refAllele; }
+                    std::cout << std::endl;
+                }
+                if (VCF2.count(pos) == 1) {
+                    fields = split(VCF2[pos], '\t'); string altAllele2 = fields[4];
+                    if (altAllele2[0]  == refSeq1[i]) { // The alternative allele in the simDia called VCF needs to match the AstCal allele
+                        std::vector<string> genotypes(fields.begin()+NUM_NON_GENOTYPE_COLUMNS,fields.end());
+                        std::cout << VCF1[pos]; print_vector_stream(genotypes, std::cout);
+                    } // else this would be multiallelic
+                }
+            }
+            else if (VCF2.count(pos) == 1) {
+                fields = split(VCF2[pos], '\t'); string altAllele2 = fields[4];
+                if (altAllele2[0]  == refSeq1[i]) { // The alternative allele in the simDia called VCF needs to match the AstCal allele
+                    std::vector<string> genotypes(fields.begin()+NUM_NON_GENOTYPE_COLUMNS,fields.end());
+                    for (int j = 0; j < NUM_NON_GENOTYPE_COLUMNS; j++) { std::cout << fields[j] << "\t"; }
+                    for (int j = 0; j < samples1.size(); j++) { std::cout << refAllele << "\t"; }
+                    print_vector_stream(genotypes,std::cout);
+                }
+            }
         }
     }
     
@@ -99,11 +190,11 @@ void parseVCFcombOptions(int argc, char** argv) {
                 exit(EXIT_SUCCESS);
         }
     }
-    if (argc - optind < 4) {
+    if (argc - optind < 5) {
         std::cerr << "missing arguments\n";
         die = true;
     }
-    else if (argc - optind > 4)
+    else if (argc - optind > 5)
     {
         std::cerr << "too many arguments\n";
         die = true;
@@ -119,4 +210,5 @@ void parseVCFcombOptions(int argc, char** argv) {
     opt::vcfFile2 = argv[optind++];
     opt::refFile1 = argv[optind++];
     opt::refFile1 = argv[optind++];
+    opt::mappabilityFile = argv[optind++];
 }
--- process_vcf_utils.h
+++ process_vcf_utils.h
@@ -358,6 +358,16 @@ template <class T> double vector_sum(T vector) {
     return sum;
 }
 
+template<typename T>
+std::vector<T> slice(std::vector<T> const &v, int m, int n)
+{
+    auto first = v.cbegin() + m;
+    auto last = v.cbegin() + n + 1;
+    
+    std::vector<T> vec(first, last);
+    return vec;
+}
+
 
 template <class T> double vector_average(T vector) {
     double sum = 0;
